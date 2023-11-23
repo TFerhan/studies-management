650620e13c4a39efe3d048989821dc295445632b
TFerhan <132760925+TFerhan@users.noreply.github.com>
2023-11-23 12:58:01 +0100

    Update app.py

--- prj/app.py
+++ prj/app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, abort, request
+from flask import Flask, render_template, abort, request, flash
 from flask import session, redirect, url_for
-
+from forms import AjtEleveForm, LoginForm
+import mysql.connector
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hola'
 
+
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'psplus14',
+    'database': 'mydb'
+}
+
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()  # Create an instance of the LoginForm
@@ -22,11 +34,43 @@ def login():
 
 @app.route("/eleves")
 def eleves():
-    return render_template("eleves.html")
+    selectionner = """
+    SELECT * FROM Eleve;
+    """
+    cursor.execute(selectionner)
+    eleves_data = cursor.fetchall()
+    return render_template("eleves.html", eleves_data = eleves_data)
 
-@app.route("/ajouter_eleve")
+
+@app.route('/ajouter_eleve', methods=['GET', 'POST'])
 def ajouter_eleve():
-    return render_template("ajouter_eleve.html")
+    form = AjtEleveForm()
+
+    if form.validate_on_submit():
+        # Retrieve form data
+        nom_ev = form.nom_ev.data
+        prenom_ev = form.prenom_ev.data
+        email_ev = form.email_ev.data
+        numtel_ev = form.numtel_ev.data
+        numtel_par = form.numtel_par.data
+        date_inscrit = form.date_inscrit.data
+        nb_absence = form.nb_absence.data
+
+        # SQL query to insert data into the table
+        insert_query = """
+        INSERT INTO Eleve (nom_ev, prenom_ev, email_ev, numtel_ev, numtel_par, date_inscrit, nb_absence)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+
+        # Execute the query with the form data
+        cursor.execute(insert_query, (nom_ev, prenom_ev, email_ev, numtel_ev, numtel_par, date_inscrit, nb_absence))
+        conn.commit()
+        flash('Ajout avec succès!', 'success')
+
+        return render_template('ajouter_eleve.html', message = "Ajouter avec success")
+
+    return render_template('ajouter_eleve.html', form=form)
+
 
 
 if __name__ == "__main__":
