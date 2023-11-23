303e7e9976f848ce2cda529209342673a7fcab68
TFerhan <132760925+TFerhan@users.noreply.github.com>
2023-11-23 12:59:20 +0100

    Update forms.py

--- prj/forms.py
+++ prj/forms.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, IntegerField, DateField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class AjtEleveForm(FlaskForm):
+    nom_ev = StringField('Nom', validators=[DataRequired(), Length(max=45)])
+    prenom_ev = StringField('Prénom', validators=[DataRequired(), Length(max=45)])
+    email_ev = StringField('Email', validators=[Email(), Length(max=45)])
+    numtel_ev = StringField('Numéro de téléphone élève', validators=[Length(max=20)])
+    numtel_par = StringField('Numéro de téléphone des parents', validators=[DataRequired(), Length(max=20)])
+    date_inscrit = DateField('Date d\'inscription', validators=[DataRequired()])
+    nb_absence = IntegerField('Nombre d\'absence', default=0, validators=[DataRequired()])
+    submit = SubmitField("Ajouter l'élève")
+
