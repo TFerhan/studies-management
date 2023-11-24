e09c73397c1038045fec31854c5f1744c6ef5c2c
TFerhan <132760925+TFerhan@users.noreply.github.com>
2023-11-24 20:22:35 +0100

    Update app.py

--- prj/app.py
+++ prj/app.py
@@ -1,22 +1,21 @@
 from flask import Flask, render_template, abort, request, flash
 from flask import session, redirect, url_for
-from forms import AjtEleveForm, LoginForm
-import mysql.connector
+from forms import AjtEleveForm
+from flask_mysqldb import MySQL
+
 
 
 app = Flask(__name__)
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_HOST'] = '127.0.0.1' # localht
+app.config['MYSQL_DB'] = 'mydb'
+
 app.config['SECRET_KEY'] = 'hola'
 
+mysql = MySQL(app)
 
-db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'hna diro lmdp lidrto f mysql rabaqi makhdamch hdchi mhm',
-    'database': 'mydb'
-}
 
-conn = mysql.connector.connect(**db_config)
-cursor = conn.cursor()
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()  # Create an instance of the LoginForm
@@ -34,11 +33,10 @@ def login():
 
 @app.route("/eleves")
 def eleves():
-    selectionner = """
-    SELECT * FROM Eleve;
-    """
-    cursor.execute(selectionner)
-    eleves_data = cursor.fetchall()
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM eleve')
+    eleves_data = cur.fetchall()
+    cur.close()
     return render_template("eleves.html", eleves_data = eleves_data)
 
 
@@ -54,20 +52,20 @@ def ajouter_eleve():
         numtel_ev = form.numtel_ev.data
         numtel_par = form.numtel_par.data
         date_inscrit = form.date_inscrit.data
-        nb_absence = form.nb_absence.data
 
         # SQL query to insert data into the table
         insert_query = """
         INSERT INTO Eleve (nom_ev, prenom_ev, email_ev, numtel_ev, numtel_par, date_inscrit, nb_absence)
         VALUES (%s, %s, %s, %s, %s, %s, %s)
         """
+        cur = mysql.connection.cursor()
 
         # Execute the query with the form data
-        cursor.execute(insert_query, (nom_ev, prenom_ev, email_ev, numtel_ev, numtel_par, date_inscrit, nb_absence))
-        conn.commit()
+        cur.execute(insert_query, (nom_ev, prenom_ev, email_ev, numtel_ev, numtel_par, date_inscrit, nb_absence))
+        mysql.connection.commit()
         flash('Ajout avec succès!', 'success')
 
-        return render_template('ajouter_eleve.html', message = "Ajouter avec success")
+        return render_template('ajouter_eleve.html')
 
     return render_template('ajouter_eleve.html', form=form)
 
