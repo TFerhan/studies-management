317ea0e9942fb7f12852fd5053839a336b1b1494
TFerhan <132760925+TFerhan@users.noreply.github.com>
2023-11-25 00:26:52 +0100

    Update app.py

--- prj/app.py
+++ prj/app.py
@@ -1,22 +1,19 @@
 from flask import Flask, render_template, abort, request, flash
 from flask import session, redirect, url_for
-from forms import AjtEleveForm, LoginForm,AjtProf
-import mysql.connector
+from forms import AjtEleveForm, AjtProf, LoginForm
+from flask_mysqldb import MySQL
+
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hola'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_HOST'] = '127.0.0.1' # localht
+app.config['MYSQL_DB'] = 'mydb'
 
+app.config['SECRET_KEY'] = 'WlUrPgXjHOL9OquLGcQ96yfoZOyNLfCYFV05NUQN9DE3Fb4tUcWW02s4pYySzeDj'
 
-db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
-    'database': 'mydb'
-}
-
-conn = mysql.connector.connect(**db_config)
-cursor = conn.cursor()
+mysql = MySQL(app)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm() 
@@ -37,44 +34,7 @@ def login():
     return render_template('login.html', form=form)
 
 
-@app.route("/eleves")
-def eleves():
-    selectionner = """
-    SELECT * FROM Eleve;
-    """
-    cursor.execute(selectionner)
-    eleves_data = cursor.fetchall()
-    return render_template("eleves.html", eleves_data = eleves_data)
 
-
-@app.route('/ajouter_eleve', methods=['GET', 'POST'])
-def ajouter_eleve():
-    form = AjtEleveForm()
-
-    if form.validate_on_submit():
-        # Retrieve form data
-        nom_ev = form.nom_ev.data
-        prenom_ev = form.prenom_ev.data
-        email_ev = form.email_ev.data
-        numtel_ev = form.numtel_ev.data
-        numtel_par = form.numtel_par.data
-        date_inscrit = form.date_inscrit.data
-        nb_absence = form.nb_absence.data
-
-        # SQL query to insert data into the table
-        insert_query = """
-        INSERT INTO Eleve (nom_ev, prenom_ev, email_ev, numtel_ev, numtel_par, date_inscrit, nb_absence)
-        VALUES (%s, %s, %s, %s, %s, %s, %s)
-        """
-
-        # Execute the query with the form data
-        cursor.execute(insert_query, (nom_ev, prenom_ev, email_ev, numtel_ev, numtel_par, date_inscrit, nb_absence))
-        conn.commit()
-        flash('Ajout avec succès!', 'success')
-
-        return render_template('ajouter_eleve.html', message = "Ajouter avec success")
-
-    return render_template('ajouter_eleve.html', form=form)
 
 @app.route('/professeur')
 def profs_list():
@@ -109,6 +69,55 @@ def ajouter_prof():
         return render_template('ajouter_prof.html', message = "Ajouter avec success",form=form)
 
     return render_template('ajouter_prof.html', form=form)
+
+
+@app.route("/eleves")
+def eleves():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM eleve')
+    eleves_data = cur.fetchall()
+    cur.close()
+    return render_template("eleves.html", eleves_data = eleves_data)
+
+@app.route('/ajouter_eleve', methods=['GET', 'POST'])
+def ajouter_eleve():
+    form = AjtEleveForm()
+
+    if form.validate_on_submit():
+        # Retrieve form data
+        nom_ev = form.nom_ev.data
+        prenom_ev = form.prenom_ev.data
+        email_ev = form.email_ev.data
+        phone_ev = form.numtel_ev.data
+        numtel_par = form.numtel_par.data
+        date_inscrit = form.date_inscrit.data
+
+        # SQL query to insert data into the table
+        insert_query = """
+        INSERT INTO Eleve (nom_ev, prenom_ev, email_ev, phone_ev, numtel_par, date_inscrit)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        """
+        cur = mysql.connection.cursor()
+
+        # Execute the query with the form data
+        cur.execute(insert_query, (nom_ev, prenom_ev, email_ev, phone_ev, numtel_par, date_inscrit))
+        mysql.connection.commit()
+        cur.close()
+        flash('Ajout avec succès!', 'success')
+
+        return redirect(url_for('eleves'))
+    return render_template('ajouter_eleve.html', form=form)
+
+
+@app.route('/delete_eleve/<int:id_eleve>', methods=['POST'])
+def delete_eleve(id_eleve):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM eleve WHERE id_eleve = %s', (id_eleve,))
+    mysql.connection.commit()
+    cur.close()
+    return redirect(url_for('eleves'))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
