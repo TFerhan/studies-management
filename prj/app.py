db1fafc55f775997e6cff05d7051f89a6cbb2232
TFerhan <132760925+TFerhan@users.noreply.github.com>
2023-12-02 21:47:42 +0100

    Update app.py

--- prj/app.py
+++ prj/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, abort, request, flash
 from flask import session, redirect, url_for
-from forms import AjtEleveForm, AjtProf, LoginForm,AjtFactureForm,AjtFormationForm,AjtGroupeForm,AjtSeanceForm
+from forms import AjtEleveForm, AjtGroupeForm, AjtSeanceForm, AjtFactureForm, AjtFormationForm, LoginForm, AjtProf, Ajtcours, Ajtseance
 from flask_mysqldb import MySQL
 
 
@@ -14,9 +14,30 @@ app.config['MYSQL_DB'] = 'mydb'
 app.config['SECRET_KEY'] = 'WlUrPgXjHOL9OquLGcQ96yfoZOyNLfCYFV05NUQN9DE3Fb4tUcWW02s4pYySzeDj'
 
 mysql = MySQL(app)
+
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/search_eleves', methods=['GET'])
+def search_eleves():
+    query = request.args.get('query', '').strip()
+
+    if query:
+        cur = mysql.connection.cursor()
+        search_query = "SELECT * FROM eleve WHERE nom_ev LIKE %s OR prenom_ev LIKE %s"
+        cur.execute(search_query, ('%' + query + '%', '%' + query + '%'))
+        eleves_data = cur.fetchall()
+        cur.close()
+    else:
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM eleve')
+        eleves_data = cur.fetchall()
+        cur.close()
+
+    return render_template("eleves.html", eleves_data=eleves_data)
+
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -32,105 +53,19 @@ def login():
         admin_data = cursor.fetchall() 
 
         if admin_data:
+            session['username'] = username
             return render_template('index.html',username=username)
         else:
-            return 'Invalid username or password. Please try again.'
+            flash('Username ou Password invalide.', 'error')
+            return render_template('login.html', form=form)
 
     return render_template('login.html', form=form)
 
 
-
-
-@app.route('/professeur')
-def profs_list():
-    cursor = mysql.connection.cursor()
-    select_prof=""" SELECT * FROM enseignant; """
-    cursor.execute(select_prof)
-    professeurs= cursor.fetchall()  
-    return render_template('profs.html', professeurs=professeurs)
-
-@app.route('/ajouter_prof', methods=['GET', 'POST'])
-def ajouter_prof():
-    form = AjtProf()
-
-    if form.validate_on_submit():
-        # Retrieve form data
-        Cin_prof = form.Cin_prof.data
-        nom_prof = form.nom_prof.data
-        prenom_prof = form.prenom_prof.data
-        email_prof = form.email_prof.data
-        numtel_prof = form.numtel_prof.data
-
-        # SQL query to insert data into the table
-        insert_query = """
-        INSERT INTO enseignant (Cin_prof, nom_enseignant, prenom_enseignant, email_enseignant, numtel_enseignant)
-        VALUES (%s, %s, %s, %s, %s)
-        """
-
-        # Execute the query with the form data
-        cursor = mysql.connection.cursor()
-        cursor.execute(insert_query, (Cin_prof, nom_prof, prenom_prof, email_prof, numtel_prof))
-        mysql.connection.commit()
-        flash('Ajout avec succès!', 'success')
-
-        return render_template('ajouter_prof.html', message = "Ajouter avec success",form=form)
-
-    return render_template('ajouter_prof.html', form=form)
-
-@app.route('/delete_prof/<int:idenseignant>', methods=['POST'])
-def delete_prof(idenseignant):
-    cur = mysql.connection.cursor()
-    cur.execute('DELETE FROM enseignant WHERE idenseignant = %s', (idenseignant,))
-    mysql.connection.commit()
-    cur.close()
-    return redirect(url_for('profs_list')) 
-
-
-@app.route('/modifier_prof/<int:idenseignant>', methods=['GET', 'POST'])
-def modifier_prof(idenseignant):
-    cursor = mysql.connection.cursor()
-    select_query = "SELECT * FROM enseignant WHERE idenseignant = %s"
-    cursor.execute(select_query, (idenseignant,))
-    professeur = cursor.fetchone()
-    cursor.close()
-
-    form = AjtProf()
-
-    if form.validate_on_submit():
-        Cin_prof = form.Cin_prof.data
-        nom_prof = form.nom_prof.data
-        prenom_prof = form.prenom_prof.data
-        email_prof = form.email_prof.data
-        numtel_prof = form.numtel_prof.data
-
-        update_query = """
-        UPDATE enseignant
-        SET Cin_prof = %s, nom_enseignant = %s, prenom_enseignant = %s,
-            email_enseignant = %s, numtel_enseignant = %s
-        WHERE idenseignant = %s
-        """
-        
-        cursor = mysql.connection.cursor()
-
-        try:
-            cursor.execute(update_query, (Cin_prof, nom_prof, prenom_prof, email_prof, numtel_prof, idenseignant))
-            mysql.connection.commit()
-
-            flash('Modification réussie!', 'success')
-            return redirect(url_for('profs_list'))  # Redirige vers la liste des professeurs
-
-        except Exception as e:
-            flash('Erreur lors de la modification!', 'error')
-
-        finally:
-            cursor.close()
-
-    return render_template('modifier_prof.html', prof=professeur, form=form)
-
-        
-    
-
-
+@app.route('/logout')
+def logout():
+    session.pop('username', None)  # Remove username from session
+    return redirect(url_for('login'))
 
 @app.route("/eleves")
 def eleves():
@@ -140,9 +75,21 @@ def eleves():
     cur.close()
     return render_template("eleves.html", eleves_data = eleves_data)
 
+
+
+
 @app.route('/ajouter_eleve', methods=['GET', 'POST'])
 def ajouter_eleve():
     form = AjtEleveForm()
+
+    # Fetch courses from the database and populate choices
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM formation")
+    formations = cur.fetchall()
+    cur.close()
+
+    # Update the choices for the cours field
+    form.formation.choices = [(formation[0], formation[1]) for formation in formations]
 
     if form.validate_on_submit():
         # Retrieve form data
@@ -152,22 +99,83 @@ def ajouter_eleve():
         phone_ev = form.numtel_ev.data
         numtel_par = form.numtel_par.data
         date_inscrit = form.date_inscrit.data
+        test = form.test.data
 
-        # SQL query to insert data into the table
+        # SQL query to insert data into the Eleve table
         insert_query = """
-        INSERT INTO Eleve (nom_ev, prenom_ev, email_ev, phone_ev, numtel_par, date_inscrit)
-        VALUES (%s, %s, %s, %s, %s, %s)
+        INSERT INTO Eleve (nom_ev, prenom_ev, email_ev, phone_ev, numtel_par, date_inscrit, test)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
         """
         cur = mysql.connection.cursor()
 
         # Execute the query with the form data
-        cur.execute(insert_query, (nom_ev, prenom_ev, email_ev, phone_ev, numtel_par, date_inscrit))
+        cur.execute(insert_query, (nom_ev, prenom_ev, email_ev, phone_ev, numtel_par, date_inscrit, test))
         mysql.connection.commit()
-        cur.close()
-        flash('Ajout avec succès!', 'success')
+        ideleve = cur.lastrowid
 
-        return redirect(url_for('eleves'))
+        selected_formation = form.formation.data
+        idformation = selected_formation[0]
+
+        cur.close()
+        flash('Ajout avec succès! ideleve={}, idformation={}'.format(ideleve, idformation), 'success')
+
+        return  redirect(url_for('ajouter_cours', ideleve=ideleve, idformation=idformation))
+
     return render_template('ajouter_eleve.html', form=form)
+
+
+@app.route('/ajouter_cours', methods=['POST', 'GET'])
+def ajouter_cours():
+    ideleve = request.args.get('ideleve')
+    idformation = request.args.get('idformation')
+    
+    
+
+    form = Ajtcours()
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * from cours where formation_idformation = %s ", (idformation,))
+    courses = cur.fetchall()
+    cur.close()
+
+    form.cours.choices = [(cours[0], cours[1]) for cours in courses]
+
+    if form.validate_on_submit():
+        selected_cours = form.cours.data
+        idcours = selected_cours[0]
+
+        # Do something with idcours if needed
+        group_disp = groupe_disponible(idcours)
+        if not group_disp:
+            flash("Tous les groupes sont remplis", "error")
+            return redirect(url_for('index'))
+
+        flash('Cours ajouté avec succès!', 'success')
+        return redirect(url_for('affectation', idcours = idcours, ideleve = ideleve, idformation = idformation, group_disp = group_disp))
+
+    return render_template('ajouter_cours.html', ideleve=ideleve, form=form, idformation=idformation)
+
+@app.route('/affectation', methods=['GET', 'POST'])
+def affectation():
+    ideleve = request.args.get('ideleve')
+    idformation = request.args.get('idformation')
+    idcours = request.args.get('idcours')
+    group_disp = request.args.get('group_disp')
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT test from eleve where ideleve = %s", (ideleve,))
+    test = cur.fetchone()
+    cur.close()
+    
+    
+
+
+
+
+def groupe_disponible(cours):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT g.idgroupe, s.jour, s.debut, s.fin FROM groupe g , séance s WHERE s.idgroupe = g.idgroupe and g.nombre_eleves < 8 and s.idcours = %s ", (cours,) )
+    groups = cur.fetchall()
+    cur.close()
+    return groups
 
 
 @app.route('/delete_eleve/<int:id_eleve>', methods=['POST'])
@@ -177,6 +185,7 @@ def delete_eleve(id_eleve):
     mysql.connection.commit()
     cur.close()
     return redirect(url_for('eleves'))
+
 
 @app.route('/groupes')
 def groupes():
@@ -346,5 +355,51 @@ def formations():
     cur.close()
     return render_template("formations.html", formations_data=formations_data)
 
+
+
+
+@app.route('/professeur')
+def profs_list():
+    cursor = mysql.connection.cursor()
+    select_prof=""" SELECT * FROM enseignant; """
+    cursor.execute(select_prof)
+    professeurs= cursor.fetchall()  
+    return render_template('profs.html', professeurs=professeurs)
+
+@app.route('/ajouter_prof', methods=['GET', 'POST'])
+def ajouter_prof():
+    form = AjtProf()
+
+    if form.validate_on_submit():
+        # Retrieve form data
+        Cin_prof = form.Cin_prof.data
+        nom_prof = form.nom_prof.data
+        prenom_prof = form.prenom_prof.data
+        email_prof = form.email_prof.data
+        numtel_prof = form.numtel_prof.data
+
+        # SQL query to insert data into the table
+        insert_query = """
+        INSERT INTO enseignant (Cin_prof, nom_enseignant, prenom_enseignant, email_enseignant, numtel_enseignant)
+        VALUES (%s, %s, %s, %s, %s)
+        """
+
+        # Execute the query with the form data
+        cursor = mysql.connection.cursor()
+        cursor.execute(insert_query, (Cin_prof, nom_prof, prenom_prof, email_prof, numtel_prof))
+        mysql.connection.commit()
+        flash('Ajout avec succès!', 'success')
+
+        return render_template('ajouter_prof.html', message = "Ajouter avec success",form=form)
+
+    return render_template('ajouter_prof.html', form=form)
+
+@app.route('/delete_prof/<int:idenseignant>', methods=['POST'])
+def delete_prof(idenseignant):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM enseignant WHERE idenseignant = %s', (idenseignant,))
+    mysql.connection.commit()
+    cur.close()
+    return redirect(url_for('profs_list')) 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=3000)
+    app.run(debug=True)
